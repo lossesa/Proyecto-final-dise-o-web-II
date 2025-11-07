47d98a72066ec30c5f3c2bf86cc54e313e5c7ee7
sara <sara24>
2025-11-06 22:11:35 -0500

    Se agregaron los cambios al repositorio. Creacion de la carpeta vite

---
+++ aplicacionGif/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "./components/Header";
+import { Search } from "./components/Search";
+import { CurrentGifs } from "./gifs/currentGifs";
+import { PreviousSearches } from "./gifs/PreviousSearches";
+
+export const App = () => {
+  return (
+    <>
+      <Header/>
+    </>
+  );
+};
---
+++ aplicacionGif/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+    const title = "Searching Gifs";
+    return (
+        <>
+            <h1>{title}</h1>
+        </>
+    );
+};
