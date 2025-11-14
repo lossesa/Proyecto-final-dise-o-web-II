b7a09dd8775dd92a77322c2cfc8047c55993b997
lossesa <sara24ramirez12@gmail.com>
2025-11-13 21:55:21 -0500

    Hay nuevos cambios.

--- aplicacionGif/src/App.jsx
+++ aplicacionGif/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Header } from "./components/Header";
-import { Search } from "./components/Search";
-import { CurrentGifs } from "./gifs/currentGifs";
-import { PreviousSearches } from "./gifs/PreviousSearches";
+import { Event } from "./components/Event";
+import { events } from "./data/events";
+// import { Search } from "./components/Search";
+// import { CurrentGifs } from "./gifs/currentGifs";
+// import { PreviousSearches } from "./gifs/PreviousSearches";
 
 export const App = () => {
   return (
